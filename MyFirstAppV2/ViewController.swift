6b364663be4fe85598c6b89f3380037b6d2f1434
Sergey Ivanov <ggdlin@gmail.com>
2020-10-13 22:51:08 +0300

    Initial Commit

---
+++ MyFirstAppV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstAppV2
+//
+//  Created by Admin on 13.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
